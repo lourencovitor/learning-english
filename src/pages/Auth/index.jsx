3265b10d0d5a18eb219fe4bfcedc2f5a8f15f573
Vitor Lourenço <vitor.brother17@outlook.com.br>
2020-11-16 14:51:39 -0300

    implemented system grid in auth to responsivity

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import {
-  Form, FormGroup, Label, Input,
+  Form,
+  FormGroup,
+  Label,
+  Input,
   Button,
+  Row,
+  Col,
 } from 'reactstrap';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { red } from '@material-ui/core/colors';
@@ -68,37 +73,53 @@ const Auth = () => {
     return setPassword(initialPassword);
   };
 
+  const onSubmit = () => {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  };
+
   return (
     <div className="main">
-      <Card className="w-25" style={{ borderRadius: '20px' }}>
-        <h1 className="text-center"><AccountCircleIcon style={{ fontSize: 50, color: red[400] }} /></h1>
-        <Form>
-          <FormGroup>
-            <Label for="email" className="mb-1">E-mail</Label>
-            <Input
-              name="email"
-              value={email.value}
-              onChange={async (e) => setEmail({ ...email, value: e.target.value })}
-              onKeyUp={() => validateEmail()}
-              valid={email.valid}
-              invalid={email.invalid}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password" className="mb-1">Password</Label>
-            <Input
-              value={password.value}
-              onChange={(e) => {
-                setPassword({ ...password, value: e.target.value });
-              }}
-              onKeyUp={() => validatePassword()}
-              valid={password.valid}
-              invalid={password.invalid}
-            />
-          </FormGroup>
-          <Button color="danger" block disabled={!email.valid || !password.valid}>Sign in</Button>
-        </Form>
-      </Card>
+      <Row className="w-100 d-flex justify-content-center">
+        <Col sm={12} md={7} lg={4}>
+          <Card style={{ borderRadius: '20px' }}>
+            <h1 className="text-center"><AccountCircleIcon style={{ fontSize: 50, color: red[400] }} /></h1>
+            <Form>
+              <FormGroup>
+                <Label for="email" className="mb-1">E-mail</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email.value}
+                  onChange={(e) => setEmail({ ...email, value: e.target.value })}
+                  onKeyUp={validateEmail}
+                  valid={email.valid}
+                  invalid={email.invalid}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="password" className="mb-1">Password</Label>
+                <Input
+                  name="password"
+                  id="password"
+                  value={password.value}
+                  onChange={(e) => {
+                    setPassword({ ...password, value: e.target.value });
+                  }}
+                  onKeyUp={() => validatePassword()}
+                  valid={password.valid}
+                  invalid={password.invalid}
+                />
+              </FormGroup>
+              <Button onClick={onSubmit} color="danger" block disabled={!email.valid || !password.valid}>Sign in</Button>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
