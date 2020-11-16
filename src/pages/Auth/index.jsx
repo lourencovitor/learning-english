a322870941b4f2f6dd82689abf640912b0ead345
Vitor Lourenço <vitor.brother17@outlook.com.br>
2020-11-16 14:39:30 -0300

    add icons in auth and finalization form

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
 import {
   Form, FormGroup, Label, Input,
+  Button,
 } from 'reactstrap';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { red } from '@material-ui/core/colors';
 import Card from '../../components/Card';
 import './index.css';
 
+const initialEmail = {
+  value: '',
+  valid: false,
+  invalid: false,
+};
+
+const initialPassword = {
+  value: '',
+  valid: false,
+  invalid: false,
+};
+
 const Auth = () => {
   const [email, setEmail] = useState({
-    value: '',
-    valid: false,
-    invalid: false,
+    initialEmail,
   });
   const [password, setPassword] = useState({
-    value: '',
-    valid: false,
-    invalid: false,
+    initialPassword,
   });
 
   const validateEmail = () => {
-    if (email.value.length) {
+    if (email) {
       const re = /\S+@\S+\.\S+/;
       const validate = re.test(email.value);
       if (validate) {
@@ -34,11 +45,11 @@ const Auth = () => {
         valid: false,
       });
     }
-    return setEmail({ ...email });
+    return setEmail(initialEmail);
   };
 
   const validatePassword = () => {
-    if (password.value.length) {
+    if (password) {
       const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
       const validate = re.test(password.value);
       if (validate) {
@@ -54,21 +65,21 @@ const Auth = () => {
         valid: false,
       });
     }
-    return setPassword({ ...password });
+    return setPassword(initialPassword);
   };
 
   return (
     <div className="main">
       <Card className="w-25" style={{ borderRadius: '20px' }}>
-        <h1 className="text-center">Sign in</h1>
+        <h1 className="text-center"><AccountCircleIcon style={{ fontSize: 50, color: red[400] }} /></h1>
         <Form>
           <FormGroup>
             <Label for="email" className="mb-1">E-mail</Label>
             <Input
               name="email"
               value={email.value}
-              onChange={(e) => setEmail({ ...email, value: e.target.value })}
-              onBlur={() => validateEmail()}
+              onChange={async (e) => setEmail({ ...email, value: e.target.value })}
+              onKeyUp={() => validateEmail()}
               valid={email.valid}
               invalid={email.invalid}
             />
@@ -80,11 +91,12 @@ const Auth = () => {
               onChange={(e) => {
                 setPassword({ ...password, value: e.target.value });
               }}
-              onBlur={() => validatePassword()}
+              onKeyUp={() => validatePassword()}
               valid={password.valid}
               invalid={password.invalid}
             />
           </FormGroup>
+          <Button color="danger" block disabled={!email.valid || !password.valid}>Sign in</Button>
         </Form>
       </Card>
     </div>
