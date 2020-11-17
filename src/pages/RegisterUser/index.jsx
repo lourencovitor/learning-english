3014bbf59eac682e21cc55f1a500a7db71a23d93
Vitor Lourenço <vitor.brother17@outlook.com.br>
2020-11-17 17:20:31 -0300

    add loading in registerUser

--- src/pages/RegisterUser/index.jsx
+++ src/pages/RegisterUser/index.jsx
@@ -1,6 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
-import { Row, Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import {
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Spinner,
+} from "reactstrap";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import { red } from "@material-ui/core/colors";
 import { Link } from "react-router-dom";
@@ -23,7 +32,7 @@ const initialValue = {
   invalid: false,
 };
 
-const RegisterUser = ({ postUserAction }) => {
+const RegisterUser = ({ postUserAction, loadingList }) => {
   const [name, setName] = useState({
     initialValue,
   });
@@ -98,135 +107,149 @@ const RegisterUser = ({ postUserAction }) => {
               <PersonAddIcon style={{ fontSize: 50, color: red[400] }} />
             </h1>
             <Form>
-              <FormGroup>
-                <Label for="name" className="mb-1">
-                  Name
-                </Label>
-                <Input
-                  type="text"
-                  name="name"
-                  id="name"
-                  value={name.value || ""}
-                  onChange={(e) => setName({ ...name, value: e.target.value })}
-                  onKeyUp={validateName}
-                  valid={name.valid}
-                  invalid={name.invalid}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="rg" className="mb-1">
-                  RG
-                </Label>
-                <Input
-                  type="number"
-                  name="rg"
-                  id="rg"
-                  max={9}
-                  value={rg.value || ""}
-                  onChange={(e) => setRg({ ...rg, value: e.target.value })}
-                  onKeyUp={validateRg}
-                  valid={rg.valid}
-                  invalid={rg.invalid}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="email" className="mb-1">
-                  E-mail
-                </Label>
-                <Input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={email.value || ""}
-                  onChange={(e) =>
-                    setEmail({ ...email, value: e.target.value })
-                  }
-                  onKeyUp={validateEmail}
-                  valid={email.valid}
-                  invalid={email.invalid}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="phone" className="mb-1">
-                  Phone
-                </Label>
-                <Input
-                  type="number"
-                  name="phone"
-                  id="phone"
-                  max={13}
-                  value={phone.value || ""}
-                  onChange={(e) =>
-                    setPhone({ ...phone, value: e.target.value })
-                  }
-                  onKeyUp={validatePhone}
-                  valid={phone.valid}
-                  invalid={phone.invalid}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="password" className="mb-1">
-                  Password
-                </Label>
-                <Input
-                  type="password"
-                  name="password"
-                  id="password"
-                  value={password.value || ""}
-                  onChange={(e) =>
-                    setPassword({ ...password, value: e.target.value })
-                  }
-                  onKeyUp={validatePassword}
-                  valid={password.valid}
-                  invalid={password.invalid}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="confirmPassword" className="mb-1">
-                  Confirm password
-                </Label>
-                <Input
-                  type="password"
-                  name="confirmPassword"
-                  id="confirmPassword"
-                  value={confirmPassword.value || ""}
-                  onChange={(e) =>
-                    setConfirmPassword({
-                      ...confirmPassword,
-                      value: e.target.value,
-                    })
-                  }
-                  onKeyUp={validateConfirmPassword}
-                  valid={confirmPassword.valid}
-                  invalid={confirmPassword.invalid}
-                />
-                <div className="d-flex justify-content-beteween mb-1">
-                  <Link to="/" className="d-flex justify-content-end w-100 ">
-                    <Button
-                      color="link"
-                      style={{ color: red[400], boxShadow: "none" }}
-                      className="d-flex justify-content-end w-100 p-2"
-                    >
-                      Sign in
-                    </Button>
-                  </Link>
+              {loadingList ? (
+                <div className="d-flex justify-content-center mt-3">
+                  <Spinner style={{ color: red[400] }} size="lg" />
                 </div>
-                <Button
-                  onClick={onSubmit}
-                  disabled={
-                    !name.valid ||
-                    !rg.valid ||
-                    !email.valid ||
-                    !phone.valid ||
-                    !password.valid ||
-                    !confirmPassword.valid
-                  }
-                  color="danger"
-                  block
-                >
-                  Sign up
-                </Button>
-              </FormGroup>
+              ) : (
+                <>
+                  <FormGroup>
+                    <Label for="name" className="mb-1">
+                      Name
+                    </Label>
+                    <Input
+                      type="text"
+                      name="name"
+                      id="name"
+                      value={name.value || ""}
+                      onChange={(e) =>
+                        setName({ ...name, value: e.target.value })
+                      }
+                      onKeyUp={validateName}
+                      valid={name.valid}
+                      invalid={name.invalid}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="rg" className="mb-1">
+                      RG
+                    </Label>
+                    <Input
+                      type="number"
+                      name="rg"
+                      id="rg"
+                      max={9}
+                      value={rg.value || ""}
+                      onChange={(e) => setRg({ ...rg, value: e.target.value })}
+                      onKeyUp={validateRg}
+                      valid={rg.valid}
+                      invalid={rg.invalid}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="email" className="mb-1">
+                      E-mail
+                    </Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={email.value || ""}
+                      onChange={(e) =>
+                        setEmail({ ...email, value: e.target.value })
+                      }
+                      onKeyUp={validateEmail}
+                      valid={email.valid}
+                      invalid={email.invalid}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="phone" className="mb-1">
+                      Phone
+                    </Label>
+                    <Input
+                      type="number"
+                      name="phone"
+                      id="phone"
+                      max={13}
+                      value={phone.value || ""}
+                      onChange={(e) =>
+                        setPhone({ ...phone, value: e.target.value })
+                      }
+                      onKeyUp={validatePhone}
+                      valid={phone.valid}
+                      invalid={phone.invalid}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="password" className="mb-1">
+                      Password
+                    </Label>
+                    <Input
+                      type="password"
+                      name="password"
+                      id="password"
+                      value={password.value || ""}
+                      onChange={(e) =>
+                        setPassword({ ...password, value: e.target.value })
+                      }
+                      onKeyUp={validatePassword}
+                      valid={password.valid}
+                      invalid={password.invalid}
+                      autoComplete="off"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="confirmPassword" className="mb-1">
+                      Confirm password
+                    </Label>
+                    <Input
+                      type="password"
+                      name="confirmPassword"
+                      id="confirmPassword"
+                      value={confirmPassword.value || ""}
+                      onChange={(e) =>
+                        setConfirmPassword({
+                          ...confirmPassword,
+                          value: e.target.value,
+                        })
+                      }
+                      onKeyUp={validateConfirmPassword}
+                      valid={confirmPassword.valid}
+                      invalid={confirmPassword.invalid}
+                    />
+                    <div className="d-flex justify-content-beteween mb-1">
+                      <Link
+                        to="/"
+                        className="d-flex justify-content-end w-100 "
+                      >
+                        <Button
+                          color="link"
+                          style={{ color: red[400], boxShadow: "none" }}
+                          className="d-flex justify-content-end w-100 p-2"
+                        >
+                          Sign in
+                        </Button>
+                      </Link>
+                    </div>
+                    <Button
+                      onClick={onSubmit}
+                      disabled={
+                        !name.valid ||
+                        !rg.valid ||
+                        !email.valid ||
+                        !phone.valid ||
+                        !password.valid ||
+                        !confirmPassword.valid
+                      }
+                      color="danger"
+                      block
+                    >
+                      Sign up
+                    </Button>
+                  </FormGroup>
+                </>
+              )}
             </Form>
           </Card>
         </Col>
